9e8ca17ff89d04fd7839281d088fa1dc3031355e
AamnaNazAwan <aamnanaz403@gmail.com>
2021-08-18 01:48:15 +0500

    Initial commit

---
+++ src/com/company/SnakeGame.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+}
+//*****************************************
+
+
+//*****************************************
+
+
